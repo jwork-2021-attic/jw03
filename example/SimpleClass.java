ee49ca47e3d53040c476c3fa505487b629537619
Chun <caochun@gmail.com>
2021-09-24 13:05:06 +0800

    update

--- example/SimpleClass.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public class SimpleClass {
-
-    public void sayHello() {
-        System.out.println("Hello");
-    }
-
-}
--- example/encoder/SteganographyFactory.java
+++ example/encoder/SteganographyFactory.java
@@ -33,34 +33,6 @@ public class SteganographyFactory {
 
         SteganographyFactory.getSteganography("example/BubbleSorter.java","example/resources/bubble.jpeg");
 
-        // File imageProvied = new File("bubble.jpeg");
-        // File fileToEncode = new File("BubbleSorter.class");
-
-        // String fileToGenerate = "bubble-sort-gen.png";
-
-        // BufferedImage image;
-        // try {
-        // FileInputStream fin = new FileInputStream(imageProvied);
-        // System.out.println(fin.available());
-        // image = ImageIO.read(imageProvied);
-        // int bitsPerColor = (int) Math.pow(2, 2);
-        // SteganographyEncoder steganographyEncoder = new SteganographyEncoder(image,
-        // bitsPerColor);
-
-        // byte[] bytes = steganographyEncoder.decodeByteArray();
-        // BytesLoader loader = new BytesLoader(Main.class.getClassLoader(), bytes);
-
-        // Class classA;
-        // classA = loader.loadClass("example.BubbleSorter");
-
-        // BufferedImage encodedImage = steganographyEncoder.encodeFile(fileToEncode);
-        // ImageIO.write(encodedImage, "png", new File(fileToGenerate));
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // } catch (ClassNotFoundException e) {
-        // e.printStackTrace();
-        // }
-
     }
 
 }
